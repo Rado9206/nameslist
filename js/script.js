eeb7f93c70def734111f29970da872c0f838676c
Radek <rj.airoo@gmail.com>
2023-06-03 23:29:27 +0200

    added js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const input = document.querySelector('.search')
+const li = document.querySelectorAll('li')
+
+const searchName = e => {
+
+	const text = e.target.value.toLowerCase()
+
+	li.forEach(el => {
+
+		if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+			el.style.display = 'block'
+		} else {
+			el.style.display = 'none'
+		}
+	})
+}
+
+input.addEventListener('keyup', searchName)
